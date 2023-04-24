1d87d0d1f94a3e7447dac75188f7bcd51da95c3f
vitaliy <vv.khritkin@gmail.com>
2023-04-24 15:44:10 +0300

    fix getting photo from cameras

--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -6,8 +6,7 @@ async def set_chat_commands(dp: Dispatcher, message: Message):
     await dp.bot.set_my_commands(commands=[
         types.BotCommand("start", "Запустить бота"),
         types.BotCommand("help", "Вывести справку"),
-        types.BotCommand("get_photo_from_cam", "Получить фото с камеры"),
-        types.BotCommand('record_from_cam', 'Записать видео с камеры')
+        types.BotCommand("get_photo_from_cam", "Получить фото с камеры")
     ],
         scope=BotCommandScopeChat(chat_id=message.chat.id)
     )
